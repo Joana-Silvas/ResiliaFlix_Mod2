9559d8d01e7bf48db3c15cc4a7a503fa46ad4603
Danielosw22 <daniel.fujita@gmail.com>
2021-05-19 03:14:20 -0300

    Ajuste de carregamento de imagens via API na pag Inicial e inicio da criação da pagina de Info

--- js/view/execPosterPagInic.js
+++ js/view/execPosterPagInic.js
@@ -1,5 +1,5 @@
 //usar o objeto e inclui no catálogo
 function usaApi(result, numeroDOM) {
-        $("#filme" + numeroDOM).attr("src", result.Poster)
+        $("#imgfilme" + numeroDOM).attr("src", result.Poster)
         $("#filme" + numeroDOM).attr("value", result.imdbID)
 }
